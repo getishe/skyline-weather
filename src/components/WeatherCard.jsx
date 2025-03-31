e6e0e742359d1ac067d1c7fdf1932aee52545fba
pc@DESKTOP-SDPG257 <getalemayhu@gmail.com>
2025-03-31 12:03:31 +0300

    Weather icon added

--- src/components/WeatherCard.jsx
+++ src/components/WeatherCard.jsx
@@ -3,6 +3,9 @@ import React from "react";
 const WeatherCard = ({ weather, unit }) => {
   if (!weather) return <p> Loading....</p>;
 
+  if (weather.cod !== 200) {
+    return <p>City not found</p>;
+  }
   return (
     <div className="mx-auto max-w-screen-xl mt-4 py-5 px-32 bg-gray-300 rounded-lg shadow-xl">
       <h1 className="text-2xl font-bold">{weather.name}</h1>
@@ -13,8 +16,8 @@ const WeatherCard = ({ weather, unit }) => {
       <p className="text-lg">Humidity: {weather.main.humidity}%</p>
       <p className="text-lg">Wind Speed: {weather.wind.speed} m/s</p>
       <img
-        src="{`http://openweathermap.org/img/wn/${weather.weather[0].`}"
-        alt="{weather.weather[0].description}"
+        src={`http://openweathermap.org/img/wn/${weather.weather[0].icon}@2x.png`}
+        alt={weather.weather[0].description}
       />
     </div>
   );
